e0bfe17666101e0f34764565071b2ff69b3696a2
Ling <lchen66@buffalo.edu>
2020-09-11 22:00:32 -0400

    #6  Added button class, color when hovering over, and Quit button functionality

--- davis.py
+++ davis.py
@@ -16,8 +16,30 @@ display_height = 768
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Davis Hall Escape Simulator 2020')
 clock = pygame.time.Clock()
-  
+
+class Button:
+
+    def __init__(self, text, color, font, pos):
+        self.text = text
+        self.color = color
+        self.font = font
+        self.pos = pos
+
+        self.set_rect()
+        self.createButton()
+
+    def buttonText(self):
+        self.rend = self.font.render(self.text, True, self.color)
  
+    def set_rect(self):
+        self.buttonText()
+        self.rect = self.rend.get_rect()
+        self.rect.center = self.pos
+
+    def createButton(self):
+        self.buttonText()
+        gameDisplay.blit(self.rend, self.rect)
+
 def render_text(text, font, color):
     textSurface = font.render(text, True, color)
     return textSurface, textSurface.get_rect()
@@ -26,34 +48,33 @@ def main_menu():
 
     intro = True
 
-    START_BUTTON, START_BUTTON_RECT = render_text("START", SPOOKY_SMALL_FONT, white)
-    OPTIONS_BUTTON, OPTIONS_BUTTON_RECT = render_text("OPTIONS",SPOOKY_SMALL_FONT, white)
-    QUIT_BUTTON, QUIT_BUTTON_RECT = render_text("EXIT", SPOOKY_SMALL_FONT, white)
+    buttons = [Button("START", white, SPOOKY_SMALL_FONT, ((display_width/2),(display_height/2))),
+    Button("OPTIONS", white, SPOOKY_SMALL_FONT, ((display_width/2),(display_height/1.5))),
+    Button("QUIT", white, SPOOKY_SMALL_FONT, ((display_width/2),(display_height/1.25)))]
 
     while intro:
+        gameDisplay.fill(black)
+        TextSurf, TextRect = render_text("Davis Hall", SPOOKY_BIG_FONT, red)
+        TextRect.center = ((display_width/2),(display_height/6))
+        gameDisplay.blit(TextSurf, TextRect)
+        
         for event in pygame.event.get():
             print(event)
-            if event.type == pygame.QUIT:
+            if (event.type == pygame.QUIT or 
+            (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and buttons[2].rect.collidepoint(pygame.mouse.get_pos()))):
                 pygame.quit()
                 quit()
                 
-        gameDisplay.fill(black)
-        TextSurf, TextRect = render_text("Davis Hall", SPOOKY_BIG_FONT, red)
-        START_BUTTON_RECT.center = ((display_width/2),(display_height/2))
-        OPTIONS_BUTTON_RECT.center = ((display_width/2),(display_height/1.5))
-        QUIT_BUTTON_RECT.center = ((display_width/2),(display_height/1.25))
-        TextRect.center = ((display_width/2),(display_height/6))
-        gameDisplay.blit(TextSurf, TextRect)
-        gameDisplay.blit(START_BUTTON, START_BUTTON_RECT)
-        gameDisplay.blit(OPTIONS_BUTTON, OPTIONS_BUTTON_RECT)
-        gameDisplay.blit(QUIT_BUTTON, QUIT_BUTTON_RECT)
+        for button in buttons:
+            if button.rect.collidepoint(pygame.mouse.get_pos()):
+                button.color = red
+            else:
+                button.color = white
+            button.createButton()
 
         pygame.display.update()
         clock.tick(15)
         
-        
-    
-    
 main_menu()
 
 
