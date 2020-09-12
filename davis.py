bc79b602980b40afdc5ca5143624288038671522
Runny Yolk <lchen66@buffalo.edu>
2020-09-11 23:38:21 -0400

    #6 #10 Clicking Options now brings you to a blank options screen with a functional back button.

--- davis.py
+++ davis.py
@@ -1,19 +1,20 @@
 import time
 import random
 import pygame
+import math
 
 pygame.init()
  
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
-SPOOKY_BIG_FONT = pygame.font.Font("assets/fonts/CHILLER.ttf", 115)
-SPOOKY_SMALL_FONT = pygame.font.Font("assets/fonts/CHILLER.ttf", 40)
+SPOOKY_BIG_FONT = pygame.font.Font("assets/fonts/CHILLER.ttf", 120)
+SPOOKY_SMALL_FONT = pygame.font.Font("assets/fonts/CHILLER.ttf", 60)
 
 display_width = 1024
 display_height = 768
  
-gameDisplay = pygame.display.set_mode((display_width,display_height))
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Davis Hall Escape Simulator 2020')
 clock = pygame.time.Clock()
 
@@ -43,20 +44,28 @@ class Button:
 def render_text(text, font, color):
     textSurface = font.render(text, True, color)
     return textSurface, textSurface.get_rect()
- 
+
+def check_Hover(button):
+    if button.rect.collidepoint(pygame.mouse.get_pos()):
+        button.color = red
+    else:
+        button.color = white
+    button.createButton()
+
 def main_menu():
 
     intro = True
 
     buttons = [Button("START", white, SPOOKY_SMALL_FONT, ((display_width/2),(display_height/2))),
     Button("OPTIONS", white, SPOOKY_SMALL_FONT, ((display_width/2),(display_height/1.5))),
-    Button("QUIT", white, SPOOKY_SMALL_FONT, ((display_width/2),(display_height/1.25)))]
+    Button("QUIT", white, SPOOKY_SMALL_FONT, ((display_width/2),(display_height/1.20)))]
+    gameDisplay.fill(black)
+    
+    TextSurf, TextRect = render_text("Davis Hall", SPOOKY_BIG_FONT, red)
+    TextRect.center = ((display_width/2),(display_height/5))
+    gameDisplay.blit(TextSurf, TextRect)
 
     while intro:
-        gameDisplay.fill(black)
-        TextSurf, TextRect = render_text("Davis Hall", SPOOKY_BIG_FONT, red)
-        TextRect.center = ((display_width/2),(display_height/6))
-        gameDisplay.blit(TextSurf, TextRect)
         
         for event in pygame.event.get():
             print(event)
@@ -64,17 +73,40 @@ def main_menu():
             (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and buttons[2].rect.collidepoint(pygame.mouse.get_pos()))):
                 pygame.quit()
                 quit()
+            
+            elif (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and buttons[1].rect.collidepoint(pygame.mouse.get_pos())):
+                options_menu()
+                
                 
         for button in buttons:
-            if button.rect.collidepoint(pygame.mouse.get_pos()):
-                button.color = red
-            else:
-                button.color = white
-            button.createButton()
+            check_Hover(button)
 
         pygame.display.update()
         clock.tick(15)
+
+def options_menu():
+    gameDisplay.fill(black)
+
+    #buttons = [Button("BACK", white, SPOOKY_SMALL_FONT, (0,0))]
+
+    backButton = Button("BACK", white, pygame.font.Font("assets/fonts/CHILLER.ttf", 70), (90, 60))
+
+    while True :
         
+        for event in pygame.event.get():
+            print(event)
+            if (event.type == pygame.QUIT) :
+                pygame.quit()
+                quit()
+
+            elif (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and backButton.rect.collidepoint(pygame.mouse.get_pos())):
+                main_menu()
+
+        check_Hover(backButton)
+
+        pygame.display.update()
+        clock.tick(15)
+
 main_menu()
 
 
