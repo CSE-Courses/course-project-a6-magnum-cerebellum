addf745a345be7b18e8ae7e79acebb5dbe6b6d6b
dmshekht <Danny12165@gmail.com>
2020-09-22 18:25:56 -0400

    #27 added config.py for commonly used constants

---
+++ config.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+SPOOKY_BIG_FONT = pygame.font.Font("assets/fonts/CHILLER.ttf", 120)
+SPOOKY_SMALL_FONT = pygame.font.Font("assets/fonts/CHILLER.ttf", 60)
+
+display_width = 1024
+display_height = 768
