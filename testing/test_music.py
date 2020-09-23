9f818cdd9f3b175a6d30688652cf9e1593d6a752
Tiffany Tate <tptate@buffalo.edu>
2020-09-23 16:25:32 -0400

    #9 test_music.py updates to test 'stop' and 'pause'/'unpause' functionality

--- testing/test_music.py
+++ testing/test_music.py
@@ -17,6 +17,28 @@ class TestMusic(unittest.TestCase):
         self.assertEqual(vol_pass, True)
         self.assertEqual(vol_fail, False)
 
+    def test_song_stop(self):
+        music_player = music.Music_Player()
+        music_player.play_intro()
+        self.assertEqual(music_player.musicMixer.get_busy(), True)
+        music_player.stop()
+        self.assertEqual(music_player.musicMixer.get_busy(), False)
+    
+    def test_song_pause(self):
+        music_player = music.Music_Player()
+        music_player.play_intro()
+        music_player.set_volume(volume = 0.0)
+        self.assertEqual(music_player.musicMixer.get_busy(), True)
+        music_player.pause()
+        pos_a = music_player.musicMixer.get_pos()
+        time.sleep(3)
+        pos_b = music_player.musicMixer.get_pos()
+        self.assertEqual(pos_a, pos_b)
+        music_player.unpause()
+        pos_c = music_player.musicMixer.get_pos()
+        time.sleep(3)
+        pos_d = music_player.musicMixer.get_pos()
+        self.assertNotEqual(pos_c, pos_d)
 
 if __name__ == '__main__':
     pygame.init()
