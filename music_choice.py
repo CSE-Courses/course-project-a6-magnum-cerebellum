9059b400e7588b927ea59a339ecf3ed888c4b2f9
montanalee <42967525+montanalee@users.noreply.github.com>
2020-12-04 23:20:36 -0500

    #140 Fixed compilation error

--- music_choice.py
+++ music_choice.py
@@ -2,19 +2,20 @@ from pygame import mixer
 import os
 import music
 import config
-import davis
+#from davis import music_player
 
 ### Will choose music to play based on several factors
 
 def encounter_choice(enemy):
+    music_player = music.Music_Player()
     type = enemy.type
     if type == "220 Student":
-        davis.music_player.play_enemy1()
+        music_player.play_enemy1()
     if type == "AI Violation Email":
-        davis.music_player.play_enemy2()
+        music_player.play_enemy2()
     if type == "Deadline":
-        davis.music_player.play_enemy3()
+        music_player.play_enemy4()
     if type == "DJ Khaled":
-        davis.music_player.play_enemy4()
+        music_player.play_enemy3()
     if type == "Wet Floor Sign":
-        davis.music_player.play_enemy5()
+        music_player.play_enemy5()
